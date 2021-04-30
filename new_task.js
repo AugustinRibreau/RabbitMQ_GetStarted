7d485e48ddd766284dacb5ae4c356b97fb44ea37
Augustin Ribreau <augustin.ribreau@gmail.com>
2021-04-30 16:26:43 +0200

    feat: 1 and 2

---
+++ new_task.js
@@ -0,0 +1,18 @@
+const queue = 'task_queue';
+const msg = process.argv.slice(2).join(' ') || "Hello World!";
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        channel.sendToQueue(queue, Buffer.from(msg), {
+            persistent: true
+        });
+        console.log(" [x] Sent '%s'", msg);
+    })
+});
---
+++ worker.js
@@ -0,0 +1,29 @@
+const queue = 'task_queue';
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+
+// This makes sure the queue is declared before attempting to consume from it
+        channel.assertQueue(queue, {
+            durable: true
+        });
+
+        channel.consume(queue, function (msg) {
+            var secs = msg.content.toString().split('.').length - 1;
+
+            console.log(" [x] Received %s", msg.content.toString());
+            setTimeout(function () {
+                console.log(" [x] Done");
+            }, secs * 1000);
+        }, {
+            // automatic acknowledgment mode,
+            // see https://www.rabbitmq.com/confirms.html for details
+            noAck: true
+        });
+
+    })
+});
